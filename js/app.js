731fc2d48d61e530da04631cde7278ec2f2b0a22
geraldelorm <geraldelorm14@gmail.com>
2021-05-31 01:51:51 +0000

    main section in progress

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const mobileNav = document.querySelector('.mobile-nav');
 const humbegger = document.querySelector('.humbegger');
 
 humbegger.addEventListener('click', navToggle);
+mobileNav.addEventListener('click', navToggle);
 
 function navToggle() {
   mobileNav.classList.toggle('opened');
